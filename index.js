0e7f5211c637da62cb83f2de61c25f276b42f8c6
Geoff Blaylock <gblaylock2024@gmail.com>
2023-03-07 23:23:34 -0800

    basic grid layout css working

---
+++ index.js
@@ -0,0 +1,8 @@
+const grid = document.querySelector(".grid-container");
+
+// function fillGrid(rows) {
+//     for (let i = 0; i < rows; i++) {
+//         grid_element = document.createElement('div');
+//         grid
+//     }
+// }
