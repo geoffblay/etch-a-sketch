78020f018437eb6e87286418ca5f9d8a8f7ac5ea
Geoff Blaylock <gblaylock2024@gmail.com>
2023-03-07 23:36:08 -0800

    fillGrid working

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const grid = document.querySelector(".grid-container");
 
-// function fillGrid(rows) {
-//     for (let i = 0; i < rows; i++) {
-//         grid_element = document.createElement('div');
-//         grid
-//     }
-// }
+function fillGrid(rows) {
+    // Clear grid
+    let child = grid.lastElementChild;
+    while (child) {
+        grid.removeChild(child);
+        child = grid.lastElementChild;
+    }
+
+    // Resize grid
+    grid.setAttribute('style', `grid-template-columns: repeat(${rows}, 1fr);`)
+
+    // Populate grid
+    for (let i = 0; i < rows * rows; i++) {
+        let grid_element = document.createElement('div');
+        grid_element.setAttribute('class', 'grid-element');
+        grid.appendChild(grid_element);
+    }
+}
+
+fillGrid(16);
