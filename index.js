826cba478d98a21f6e0199942f266e0bd346d58e
Geoff Blaylock <gblaylock2024@gmail.com>
2023-03-08 00:32:04 -0800

    resize grid working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector(".grid-container");
 const reload = document.querySelector("#reset-btn");
+const resize = document.querySelector("#resize-btn");
 
 reload.addEventListener('click', clearColor);
+resize.addEventListener('click', getNumRows);
 
 document.addEventListener('DOMContentLoaded', fillGrid(16))
+
+
 
 function fillGrid(rows) {
     // Clear grid
@@ -37,4 +41,13 @@ function clearColor() {
     pixels.forEach((pixel) => {
         pixel.setAttribute('style', 'background-color: white');
     })
+}
+
+function getNumRows() {
+    let num_rows = prompt("Enter number of pixels per side : ");
+    while (num_rows > 100) {
+        num_rows = prompt("Oops! The maximum pixels per side is 100. "
+                            + "Please enter a number 1 - 100: ")
+    }
+    fillGrid(num_rows);
 }
